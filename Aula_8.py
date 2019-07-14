9dac6dd4fd7f4dc70a903364a20509068ab735a1
Raffael Bino <raffael_bino18yahoo.com.br>
2019-07-14 00:57:30 -0300

    add nova aula e desafios

--- Aula_8.py
+++ Aula_8.py
@@ -1,4 +1,14 @@
-import math
-n = int(input('Digite um número: '))
-raiz = math.sqrt(n)
-print('A raiz quadradra de {} é {:.2f}'.format(n,raiz))
+# MATH
+# import math # Importa tudo
+#from math import sqrt # Importa so o sqrt do math
+#n = int(input('Digite um número: '))
+#raiz = sqrt(n)
+#print('A raiz quadradra de {} é {:.2f}'.format(n, raiz))
+
+# RANDOM
+#import random
+#n = random.random()
+#n = random.randint(1, 10)
+#print(n)
+import emoji
+print(emoji.emojize('Olá mundo :earth_americas:', use_aliases=True))

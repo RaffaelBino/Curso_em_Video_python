211750bf3eec30ddc6978ef161ead2fa6e46ca1c
Raffael Bino <raffael_bino18yahoo.com.br>
2019-07-27 11:40:47 -0300

    Add exercios de string

---
+++ Exercicio_p_strings.py
@@ -0,0 +1,10 @@
+# Exercicio 22
+print('='*20, 'Primeiro exercicio','='*20)
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Análisando seu nome...')
+print('Seu nome em maiusculo é: {}'.format(nome.upper()))
+print('Seu nome em minusculo é: {}'.format(nome.lower()))
+print('Seu nome tem {} letras'.format(len(nome) - nome.count(' ')))
+# print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e tem {} letras'.format(separa[0], len(separa[0])))

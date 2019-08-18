07ff499798457e65cb5431a66d86e39c4a3d8cbe
Raffael Bino <raffael_bino18yahoo.com.br>
2019-08-18 12:50:31 -0300

    Add exerc for

--- Exercicio_estrut_rept_for.py
+++ Exercicio_estrut_rept_for.py
@@ -1,14 +1,15 @@
 from time import sleep
 # Exercicio 46
-print('='*20, 'Primeiro exercicio','='*20)
+print('=' * 20, 'Primeiro exercicio', '=' * 20)
 for i in range(10, -1, -1):
     print(i)
     sleep(.5)
 print('FELIZ ANO NOVO!!!')
 
 # Exercicio 47
-print('='*20, 'Segundo exercicio','='*20)
-for i in range(0, 51):
-    if i % 2 == 0:
-        print(i, end=' ')
+print('=' * 20, 'Segundo exercicio', '=' * 20)
+for i in range(0, 51, 2):
+    # for i in range(0, 51):
+    #    if i % 2 == 0:
+    print(i, end=' ')
 print('Acabou')

5a31d96667d088ed09c41dd4abffe22bd24ec20b
Raffael Bino <raffael_bino18yahoo.com.br>
2019-08-18 12:47:00 -0300

    Add exerc for

--- Exercicio_estrut_rept_for.py
+++ Exercicio_estrut_rept_for.py
@@ -5,3 +5,10 @@ for i in range(10, -1, -1):
     print(i)
     sleep(.5)
 print('FELIZ ANO NOVO!!!')
+
+# Exercicio 47
+print('='*20, 'Segundo exercicio','='*20)
+for i in range(0, 51):
+    if i % 2 == 0:
+        print(i, end=' ')
+print('Acabou')

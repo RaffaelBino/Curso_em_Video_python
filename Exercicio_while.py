1eac9ebc77ceaefa44efaa68820f701f2844dbef
unknown <raffael_bino18@yahoo.com.br>
2019-08-27 13:23:38 -0300

    add exerc while

--- Exercicio_while.py
+++ Exercicio_while.py
@@ -1,6 +1,9 @@
 # Exercicio 57
 print('=' * 20, 'Primeiro exercicio', '=' * 20)
-sexo = str(input('Digite seu sexo: [M/F] ')).upper()
-while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Dados incorretos! Por favor digite M ou F: ')).upper()
+sexo = str(input('Digite seu sexo: [M/F] ')).upper().strip()[0]  # O [0] serve para pegar apenas a primeira letra do que for digitado
+while sexo not in 'MmFf':
+    sexo = str(input('Dados incorretos! Por favor digite M ou F: ')).upper().strip()
 print('Sexo {} registrado com sucesso!'.format(sexo))
+
+# Exercicio 58
+print('=' * 20, 'Segundo exercicio', '=' * 20)

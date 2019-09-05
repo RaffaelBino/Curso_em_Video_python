26faeaa00686fa3c20f09dcd309ceec35b6edda9
unknown <raffael_bino18@yahoo.com.br>
2019-09-05 19:34:19 -0300

    add exerc while c break

--- exercicios_while_c_break.py
+++ exercicios_while_c_break.py
@@ -95,3 +95,27 @@ while True:
 print(f'O total da compra foi: {precotot}')
 print(f'Temos {cust1000} produto(s) custando mais de R$1000.00')
 print(f'O produto mais barato é {barato} que custou R${menor:.2f}')
+
+# Exercicio 71
+print('=' * 20, 'Sexto exercicio', '=' * 20)
+print('BANCO')
+saque = int(input('Quanto você quer sacar ? R$'))
+tot = saque
+ced = 50
+totced = 0
+while True:
+    if tot >= ced:
+        tot -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'O total de {totced} cédulas de R${ced}')
+            if ced == 50:
+                ced = 20
+            elif ced == 20:
+                ced = 10
+            elif ced == 10:
+                ced = 1
+            totced = 0
+            if tot == 0:
+                break
